5f946342fce4bd8afcfe83b04f675aa896a3942b
석경우 <rudtyz@gmail.com>
2020-01-19 21:29:19 +0900

    print facade

--- src/main/java/org/rudtyz/serious/multiplication/service/MultiplicationEventService.java
+++ src/main/java/org/rudtyz/serious/multiplication/service/MultiplicationEventService.java
@@ -1,33 +1,20 @@
 package org.rudtyz.serious.multiplication.service;
 
 import org.rudtyz.serious.multiplication.service.event.IntegerMultiplicationEvent;
-import org.rudtyz.serious.multiplication.service.factory.*;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MultiplicationEventService {
 
-    private final StringPrinter stringPrinter;
-    private final IntegerPrinter integerPrinter;
-    private final NewLinePrinter newLinePrinter;
+    private final MultiplicationPrintFacade multiplicationPrintFacade;
 
-    public MultiplicationEventService(
-            StringPrinterFactory stringPrinterFactory,
-            IntegerPrinterFactory integerPrinterFactory,
-            NewLinePrinterFactory newLinePrinterFactory) {
-        this.stringPrinter = stringPrinterFactory.create();
-        this.integerPrinter = integerPrinterFactory.create();
-        this.newLinePrinter = newLinePrinterFactory.create();
+    public MultiplicationEventService(MultiplicationPrintFacade multiplicationPrintFacade) {
+        this.multiplicationPrintFacade = multiplicationPrintFacade;
     }
 
     @EventListener
     public void onMultiplicationEventReceive(IntegerMultiplicationEvent event) {
-        integerPrinter.print(event.getLhs());
-        stringPrinter.print(" * ");
-        integerPrinter.print(event.getRhs());
-        stringPrinter.print(" = ");
-        integerPrinter.print(event.getResult());
-        newLinePrinter.print();
+        multiplicationPrintFacade.print(event.getLhs(), event.getRhs(), event.getResult());
     }
 }
---
+++ src/main/java/org/rudtyz/serious/multiplication/service/MultiplicationPrintFacade.java
@@ -0,0 +1,29 @@
+package org.rudtyz.serious.multiplication.service;
+
+import org.rudtyz.serious.multiplication.service.factory.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MultiplicationPrintFacade {
+    private final StringPrinter stringPrinter;
+    private final IntegerPrinter integerPrinter;
+    private final NewLinePrinter newLinePrinter;
+
+    public MultiplicationPrintFacade(
+            StringPrinterFactory stringPrinterFactory,
+            IntegerPrinterFactory integerPrinterFactory,
+            NewLinePrinterFactory newLinePrinterFactory) {
+        this.stringPrinter = stringPrinterFactory.create();
+        this.integerPrinter = integerPrinterFactory.create();
+        this.newLinePrinter = newLinePrinterFactory.create();
+    }
+
+    public void print(int lhs, int rhs, int result) {
+        integerPrinter.print(lhs);
+        stringPrinter.print(" * ");
+        integerPrinter.print(rhs);
+        stringPrinter.print(" = ");
+        integerPrinter.print(result);
+        newLinePrinter.print();
+    }
+}
