8ee9073f21b9bdcdcd0e6f4e92f66b88dfe9242d
석경우 <rudtyz@gmail.com>
2020-01-25 18:54:54 +0900

    MultiplicationEventService 다른 스레드에서 동작

--- src/main/java/org/rudtyz/serious/multiplication/service/support/MultiplicationEventService.java
+++ src/main/java/org/rudtyz/serious/multiplication/service/support/MultiplicationEventService.java
@@ -1,6 +1,7 @@
 package org.rudtyz.serious.multiplication.service.support;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,7 @@ public class MultiplicationEventService {
         this.multiplicationPrintFacade = multiplicationPrintFacade;
     }
 
+    @Async
     @EventListener
     public void onMultiplicationEventReceive(NumberMultiplicationEvent event) {
         multiplicationPrintFacade.print(event.getLhs(), event.getRhs(), event.getResult());
